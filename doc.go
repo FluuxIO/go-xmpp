b9b77f6be90ac449f5b9780d8fcc0b5237cbcb15
Mickael Remond <mremond@process-one.net>
2018-01-25 23:02:01 +0100

    Improve code documentation

--- doc.go
+++ doc.go
@@ -7,5 +7,24 @@ The goal is to make simple to write simple adhoc XMPP clients:
  - For building connected "things" by plugging them on an XMPP server,
  - For writing simple chatbots to control a service or a thing.
 
+Fluux XMPP can be used to build XMPP clients or XMPP components.
+
+Clients
+
+Fluux XMPP can be use to create fully interactive XMPP clients (for
+example console-based), but it is more commonly used to build automated
+clients (connected devices, automation scripts, chatbots, etc.).
+
+Components
+
+XMPP components can typically be used to extends the features of an XMPP
+server, in a portable way, using component protocol over persistent TCP
+connections.
+
+Compliance
+
+Fluux XMPP has been primarily tested with [ejabberd](https://www.ejabberd.im)
+but it should work with any XMPP compliant server.
+
 */
 package xmpp
