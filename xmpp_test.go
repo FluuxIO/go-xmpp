ffcde39ba658fde01030bb0dad38a65cea14c9e2
Mickaël Rémond <mremond@process-one.net>
2019-02-10 17:53:18 +0100

    Add test (and refactor them) for PR#15 (#18)
    * Add test (and refactor them) for #15
    * Update Dockerfile to support Go modules on Codeship

---
+++ xmpp_test.go
@@ -0,0 +1,29 @@
+package xmpp_test // import "gosrc.io/xmpp_test"
+
+import (
+	"encoding/xml"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// Compare iq structure but ignore empty namespace as they are set properly on
+// marshal / unmarshal. There is no need to manage them on the manually
+// crafted structure.
+func xmlEqual(x, y interface{}) bool {
+	alwaysEqual := cmp.Comparer(func(_, _ interface{}) bool { return true })
+	opts := cmp.Options{
+		cmp.FilterValues(func(x, y interface{}) bool {
+			xx, xok := x.(xml.Name)
+			yy, yok := y.(xml.Name)
+			if xok && yok {
+				zero := xml.Name{}
+				if xx == zero || yy == zero {
+					return true
+				}
+			}
+			return false
+		}, alwaysEqual),
+	}
+
+	return cmp.Equal(x, y, opts)
+}
