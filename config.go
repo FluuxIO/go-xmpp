996feb1a408f3a27a92198787f11905c44cd5ca8
Mickael Remond <mremond@process-one.net>
2019-05-31 19:11:15 +0200

    Better documentation for config

--- config.go
+++ config.go
@@ -10,10 +10,10 @@ type Config struct {
 	Jid            string
 	parsedJid      *Jid // For easier manipulation
 	Password       string
-	PacketLogger   *os.File // Used for debugging
-	Lang           string   // TODO: should default to 'en'
-	Retry          int      // Number of retries for connect
-	ConnectTimeout int      // Connection timeout in seconds. Default to 15
-	Insecure       bool     // set to true to allow comms without TLS
+	PacketLogger   *os.File                                                 // Used for debugging
+	Lang           string                                                   // TODO: should default to 'en'
+	Retry          int                                                      // Number of retries for connect
+	ConnectTimeout int                                                      // Connection timeout in seconds. Default to 15
+	Insecure       bool                                                     // set to true to allow comms without TLS
 	CharsetReader  func(charset string, input io.Reader) (io.Reader, error) // passed to xml decoder
 }
