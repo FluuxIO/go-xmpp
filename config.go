e54260ec68da242b7400205f1317bd34f8cf67ba
Mickael Remond <mremond@process-one.net>
2019-05-31 19:22:36 +0200

    Clarify use of insecure flag

--- config.go
+++ config.go
@@ -10,10 +10,12 @@ type Config struct {
 	Jid            string
 	parsedJid      *Jid // For easier manipulation
 	Password       string
-	PacketLogger   *os.File                                                 // Used for debugging
-	Lang           string                                                   // TODO: should default to 'en'
-	Retry          int                                                      // Number of retries for connect
-	ConnectTimeout int                                                      // Connection timeout in seconds. Default to 15
-	Insecure       bool                                                     // set to true to allow comms without TLS
-	CharsetReader  func(charset string, input io.Reader) (io.Reader, error) // passed to xml decoder
+	PacketLogger   *os.File // Used for debugging
+	Lang           string   // TODO: should default to 'en'
+	Retry          int      // Number of retries for connect
+	ConnectTimeout int      // Connection timeout in seconds. Default to 15
+	// Insecure can be set to true to allow to open a session without TLS. If TLS
+	// is supported on the server, we will still try to use it.
+	Insecure      bool
+	CharsetReader func(charset string, input io.Reader) (io.Reader, error) // passed to xml decoder
 }
