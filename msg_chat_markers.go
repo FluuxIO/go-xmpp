687d9b37fd21bc59eca546715eb1f55b3af60f1e
Mickael Remond <mremond@process-one.net>
2019-06-04 18:27:32 +0200

    Add support for chat markers parsing (XEP-0333)

---
+++ msg_chat_markers.go
@@ -0,0 +1,24 @@
+package xmpp
+
+import "encoding/xml"
+
+/*
+Support for:
+- XEP-0333 - Chat Markers: https://xmpp.org/extensions/xep-0333.html
+*/
+
+type Markable struct {
+	MsgExtension
+	XMLName xml.Name `xml:"urn:xmpp:chat-markers:0 markable"`
+}
+
+type MarkReceived struct {
+	MsgExtension
+	XMLName xml.Name `xml:"urn:xmpp:chat-markers:0 received"`
+	ID      string
+}
+
+func init() {
+	typeRegistry.MapExtension(PKTMessage, xml.Name{"urn:xmpp:chat-markers:0", "markable"}, Markable{})
+	typeRegistry.MapExtension(PKTMessage, xml.Name{"urn:xmpp:chat-markers:0", "received"}, MarkReceived{})
+}
