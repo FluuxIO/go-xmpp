5477997a38b217c86c462c8bfa8d96c3b925c1f5
Martin/Geno <geno+dev@fireorbit.de>
2019-06-05 03:58:51 +0200

    complete XEP-0333 support (with displayed)

--- msg_chat_markers.go
+++ msg_chat_markers.go
@@ -20,7 +20,21 @@ type MarkReceived struct {
 	ID      string
 }
 
+type MarkDisplayed struct {
+	MsgExtension
+	XMLName xml.Name `xml:"urn:xmpp:chat-markers:0 displayed"`
+	ID      string
+}
+
+type MarkAcknowledged struct {
+	MsgExtension
+	XMLName xml.Name `xml:"urn:xmpp:chat-markers:0 acknowledged"`
+	ID      string
+}
+
 func init() {
 	typeRegistry.MapExtension(PKTMessage, xml.Name{NSMsgChatMarkers, "markable"}, Markable{})
 	typeRegistry.MapExtension(PKTMessage, xml.Name{NSMsgChatMarkers, "received"}, MarkReceived{})
+	typeRegistry.MapExtension(PKTMessage, xml.Name{NSMsgChatMarkers, "displayed"}, MarkDisplayed{})
+	typeRegistry.MapExtension(PKTMessage, xml.Name{NSMsgChatMarkers, "acknowledged"}, MarkAcknowledged{})
 }
