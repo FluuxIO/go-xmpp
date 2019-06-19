95bbbcc45e668fc0c157e16c4e18f8815c1c6d5a
Mickael Remond <mremond@process-one.net>
2019-06-19 19:33:31 +0200

    For now, we do not need to expose backoff in the documentation

--- backoff_test.go
+++ backoff_test.go
@@ -1,21 +1,19 @@
-package xmpp_test
+package xmpp
 
 import (
 	"testing"
 	"time"
-
-	"gosrc.io/xmpp"
 )
 
 func TestDurationForAttempt_NoJitter(t *testing.T) {
-	b := xmpp.Backoff{Base: 25, NoJitter: true}
+	b := backoff{Base: 25, NoJitter: true}
 	bInMS := time.Duration(b.Base) * time.Millisecond
-	if b.DurationForAttempt(0) != bInMS {
-		t.Errorf("incorrect default duration for attempt #0 (%d) = %d", b.DurationForAttempt(0)/time.Millisecond, bInMS/time.Millisecond)
+	if b.durationForAttempt(0) != bInMS {
+		t.Errorf("incorrect default duration for attempt #0 (%d) = %d", b.durationForAttempt(0)/time.Millisecond, bInMS/time.Millisecond)
 	}
 	var prevDuration, d time.Duration
 	for i := 0; i < 10; i++ {
-		d = b.DurationForAttempt(i)
+		d = b.durationForAttempt(i)
 		if !(d >= prevDuration) {
 			t.Errorf("duration should be increasing between attempts. #%d (%d) > %d", i, d, prevDuration)
 		}
