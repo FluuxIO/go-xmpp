f79a3a219bfae1368ee12e07e28a9f49c349a500
Mickael Remond <mremond@process-one.net>
2019-06-26 14:00:39 +0200

    Improves IPV6 examples

--- network_test.go
+++ network_test.go
@@ -17,11 +17,11 @@ func TestParseAddr(t *testing.T) {
 		{name: "ipv4-with-port-1", input: "localhost:5555", want: "localhost:5555"},
 		{name: "ipv4-no-port-2", input: "127.0.0.1", want: "127.0.0.1:5222"},
 		{name: "ipv4-with-port-2", input: "127.0.0.1:5555", want: "127.0.0.1:5555"},
-		{name: "ipv6-no-port-1", input: "1::", want: "[1::]:5222"},
-		{name: "ipv6-no-port-2", input: "[1::]", want: "[1::]:5222"},
-		{name: "ipv6-no-port-3", input: "1::2008", want: "[1::2008]:5222"},
-		{name: "ipv6-no-port-4", input: "1::2008:1", want: "[1::2008:1]:5222"},
-		{name: "ipv6-with-port-1", input: "[1::]:5555", want: "[1::]:5555"},
+		{name: "ipv6-no-port-1", input: "::1", want: "[::1]:5222"},
+		{name: "ipv6-no-port-2", input: "[::1]", want: "[::1]:5222"},
+		{name: "ipv6-no-port-3", input: "2001::7334", want: "[2001::7334]:5222"},
+		{name: "ipv6-no-port-4", input: "2001:db8:85a3:0:0:8a2e:370:7334", want: "[2001:db8:85a3:0:0:8a2e:370:7334]:5222"},
+		{name: "ipv6-with-port-1", input: "[::1]:5555", want: "[::1]:5555"},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(st *testing.T) {
